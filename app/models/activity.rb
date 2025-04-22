f4d6e29acae96e27df2d3518a7933b358bffef77
Hassan Ali <haxxanali512@gmail.com>
2025-04-22 21:01:32 +0500

    update

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,6 @@
 class Activity < ApplicationRecord
   belongs_to :project
   belongs_to :user
+
+  validates :body, presence: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
   has_many :activities, dependent: :destroy
+  validates_presence_of :title, :description, :start_date, :due_date
 end
